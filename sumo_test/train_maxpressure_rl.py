af977c87e879aee091ee1aaaba8767c57d4f8860
RaulTheMiner17 <raulferns1@gmail..com>
2026-05-07 20:54:03 +0530

    cleanup

--- sumo_test/train_maxpressure_rl.py
+++ sumo_test/train_maxpressure_rl.py
@@ -257,7 +257,7 @@ def linear_schedule(initial_lr: float, final_lr: float = 1e-5):
 
 
 def train_best_model():
-    TOTAL_TIMESTEPS = 500_000
+    TOTAL_TIMESTEPS = 1000_000
 
     # ------------------------------------------------------------------
     # 1. Vectorised training environment
